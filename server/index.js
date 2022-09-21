8ee87aecb587e3afcbc366d79ac2ded5a0ad50cd
Shahina <shahina@hamon.in>
2022-09-21 15:34:44 +0530

    modified

--- server/index.js
+++ server/index.js
@@ -70,6 +70,6 @@ app.delete("/todos/:id", async(req, res) => {
   }
 })
 
-app.listen(3000, () => {
-    console.log("server has started on port 3000");
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
 });
